d9d76b593deb813b668a0a13ba1235b7ac4ff209
Nguyen Trong Phuc <kobaogiopo2@gmail.com>
2022-02-27 08:56:05 +0700

    add client folder

--- server/index.js
+++ server/index.js
@@ -6,21 +6,34 @@ const authRoute = require("./routes/api_auth_routes");
 const cookieParser = require('cookie-parser')
 
 
-dotenv.config();
-connectDB();
+//socket
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+
 
 const app = express();
 const port = process.env.PORT || 3333
+const httpServer = createServer(app);
+const io = new Server(httpServer, { /* options */ });
+
 
 // Configs
 app.use(express.json())
 app.use(cookieParser());
+dotenv.config();
+connectDB();
 
 
 //Router
 authRoute(app);
 
-app.listen(port, () => console.log("Server is runing with port " + port))
+//socket
+
+io.on("connection", (socket) => {
+  console.log(socket.id);
+});
+
+httpServer.listen(port, () => console.log("Server is runing with port " + port))
 
 
 
